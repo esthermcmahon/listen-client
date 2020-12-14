36a8375235d1989488543ba759c319cc783ab7aa
estherviolin <esthernaomi.violin@gmail.com>
2020-12-14 15:32:20 -0600

    getting secure url from cloudinary

--- src/components/Recordings/NewRecording.js
+++ src/components/Recordings/NewRecording.js
@@ -3,120 +3,115 @@ import 'react-voice-recorder/dist/index.css'
 import { RecordingContext } from "./RecordingProvider"
 import React, { useContext, useState, useEffect } from "react"
 import {
-    Box,
-    Button,
-    TextInput,
-    Text
-  } from "grommet";
-
-
+  Box,
+  Button,
+  TextInput,
+  Text
+} from "grommet"
+// import { Audio, Transformation, CloudinaryContext } from 'cloudinary-react'
 
 
 export const NewRecording = (props) => {
-//set state state var and setter function 
-// use Effect when recording ends, reset state
+  //set state state var and setter function 
+  // use Effect when recording ends, reset state
 
-const {getRecordings, createRecording } = useContext(RecordingContext)
+  const { getRecordings, createRecording } = useContext(RecordingContext)
+  const [currentRecording, setCurrentRecording] = useState({})
+  const excerptId = props.match.params.excerptId
 
-const [currentRecording, setCurrentRecording] = useState({})
+  const [audio, setAudio] = useState('')
+  const [loading, setLoading] = useState(false)
 
-const excerptId = props.match.params.excerptId
-
-const [audio, setAudio] = useState('')
-const [loading, setLoading] = useState(false)
-
-useEffect(() => {
+  useEffect(() => {
     getRecordings()
-}, [])
+  }, [])
 
-const handleChange = (event) => {
+  const handleChange = (event) => {
     const newRecordingState = Object.assign({}, currentRecording)
     newRecordingState[event.target.name] = event.target.value
     setCurrentRecording(newRecordingState)
   }
   const jsonDate = new Date(Date.now()).toJSON().slice(0, 10);
-  
+
   const constructNewRecording = () => {
-    const newRecording = { 
-        label: currentRecording.label,
-        excerpt: excerptId,
-        audio: recorderState.url,
-        date: jsonDate
-    
+    const newRecording = {
+      label: currentRecording.label,
+      excerpt: excerptId,
+      audio: audio,
+      date: jsonDate
+
     }
+    // recorderState.url
+
     createRecording(newRecording)
-        .then(() => props.history.push("/home"))
-}
+      .then(() => props.history.push("/home"))
+  }
 
-var cloudinary_api = "https://api.cloudinary.com/v1_1/dkicrisrl/mh/upload"
-var cloudinary_upload_preset = "zv6murma/mh/upload"
+  // var cloudinary_api = "https://api.cloudinary.com/v1_1/dkicrisrl/mh/upload"
+  // var cloudinary_upload_preset = "zv6murma/raw/upload"
 
-// const uploadAudio = async e => {
-//     const files = e.target.files
-//     const data = new FormData()
-//     data.append('file', files[0])
-//     data.append(cloudinary_upload_preset, 'listen')
-//     setLoading(true)
-//     const res = await fetch(
-//         "https://api.cloudinary.com/v1_1/dkicrisrl/mh/upload",
-//         {
-//             method: "POST",
-//             body: data
-//         }
-//     )
-//     const file = await res.json()
-//     setAudio(file.secure_url)
-//     setLoading(false)
-//     console.log(audio)
-// }
 
-const uploadAudio = async url => {
-    const data = new FormData()
-    data.append('url', url)
-    data.append(cloudinary_upload_preset, 'listen')
-    setLoading(true)
-    debugger
-    const res = await fetch(
-        "https://api.cloudinary.com/v1_1/dkicrisrl/mh/upload", {
-            method: "POST",
-            mode: "no-cors",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: data
-        })
-        const file = res.json()
-        setAudio(file.secure_url)
-        setLoading(false)
-        console.log(audio) 
+  const uploadAudio = () => {
+    var reader = new FileReader()
+
+    reader.readAsDataURL(recorderState)
+    reader.onloadend = function () {
+      var base64data = reader.result
+      // console.log(base64data)
+      const data = new FormData()
+      data.append('file', base64data)
+      data.append('upload_preset', 'zv6murma')
+      data.append('resource_type', 'video')
+      fetch("https://api.cloudinary.com/v1_1/dkicrisrl/upload", {
+        method: "POST",
+        headers: {
+          "Accept": "application/json"
+
+        },
+        body: data
+      })
+        .then(res => res.json())
+        .then(res => {
+          const audioresult = res
+          setAudio(audioresult.secure_url)
+          // setAudio(data.secure_url)
+          console.log(audio)
+        })   
+
+    }
+  }
+
+  const [recorderState, setRecorderState] = useState({
+    url: null,
+    blob: null,
+    chunks: null,
+    duration: {
+      h: 0,
+      m: 0,
+      s: 0
+    }
+  })
+
+  useEffect(() => {
+    if (recorderState.blob !== null) {
+      uploadAudio()
     }
     
+  }, [recorderState])
 
-    
-const [recorderState, setRecorderState] = useState({
-  url: null,
-  blob: null,
-  chunks: null,
-  duration: {
-    h: 0,
-    m: 0,
-    s: 0
-  }
-})
-
-
-const handleAudioStop = (data) => {
+  const handleAudioStop = (data) => {
     console.log(data)
-    setRecorderState(data)
-    
+    // setRecorderState(data)
+
   }
 
-const handleAudioUpload = (file) => {
-    uploadAudio(recorderState.url)
+  const handleAudioUpload = (file) => {
+    setRecorderState(file)
+    console.log(recorderState)
     console.log(file);
   }
 
-const handleReset = () => {
+  const handleReset = () => {
     const reset = {
       url: null,
       blob: null,
@@ -131,46 +126,49 @@ const handleReset = () => {
   }
 
 
-return (
-   <>
-        <Recorder
-        record = { true }
-        title = { "New recording" }
-        audioURL = {recorderState.url}
+  return (
+    <>
+      <Recorder
+        record={true}
+        title={"New recording"}
+        audioURL={recorderState.url}
         showUIAudio
-        handleAudioStop = {data => handleAudioStop(data) }
-        handleAudioUpload = {data => handleAudioUpload(data)}
-        handleRest = {() => handleReset()}
+        handleAudioStop={data => handleAudioStop(data)}
+        handleAudioUpload={data => handleAudioUpload(data)}
+        handleRest={() => handleReset()}
 
-        />
+      />
 
-        <Box>
-            <Text htmlFor="label">Enter excerpt name and take: </Text>
-            <Box margin="small">
-                <TextInput
-                type="text"
-                name="label"
-                required
-                autoFocus
-                placeholder="ex: Excerpt take 1"
-                value={currentRecording.label}
-                onChange={handleChange}
-                />
-            </Box>
+      {/* <Audio src={recorderState.url} cloudName="dkicrisrl" controls publicId="dkicrisrl" type="ogg" onClick={uploadAudio} /> */}
+
+
+      <Box>
+        <Text htmlFor="label">Enter excerpt name and take: </Text>
+        <Box margin="small">
+          <TextInput
+            type="text"
+            name="label"
+            required
+            autoFocus
+            placeholder="ex: Excerpt take 1"
+            value={currentRecording.label}
+            onChange={handleChange}
+          />
         </Box>
-        <Button
-              fill={false}
-              primary
-              label="Save"
-              onClick={(evt) => {
-                constructNewRecording(evt)
-                ;
-              }}
-            />
+      </Box>
+      <Button
+        fill={false}
+        primary
+        label="Save"
+        onClick={(evt) => {
+          constructNewRecording(evt)
+            ;
+        }}
+      />
 
-       
-    </> 
-)
+
+    </>
+  )
 
 
 }
